7359c164ff555f2c6e479fce42ddab367443d996
Manonmani13 <manonmani.pandukumar@gmail.com>
2025-05-17 15:58:27 +0530

    microservice

---
+++ sb_microservice_registory/src/main/java/com/example/sb_microservice_registory/SbMicroserviceRegistoryApplication.java
@@ -0,0 +1,13 @@
+package com.example.sb_microservice_registory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbMicroserviceRegistoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbMicroserviceRegistoryApplication.class, args);
+	}
+
+}
---
+++ sb_microservice_registory/src/test/java/com/example/sb_microservice_registory/SbMicroserviceRegistoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sb_microservice_registory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbMicroserviceRegistoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
